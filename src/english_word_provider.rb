7cfa1d59e799b4d01ab6dc24aa5a66516f4d0a0b
sejtenik <sejtenik@gmail.com>
2025-02-04 20:52:38 +0100

    added wordreference

--- src/english_word_provider.rb
+++ src/english_word_provider.rb
@@ -223,3 +223,35 @@ class LongmanParser < EnglishWordProvider
   end
 
 end
+
+
+class WordReferenceParser < EnglishWordProvider
+  def fetch_word(doc)
+    doc.at_css('.post-img a')['alt'].split(':').last.strip
+  end
+
+  def fetch_definitions(doc, word)
+    link = doc.at_css('.post-img a')['href']
+    word_url = Addressable::URI.parse(link).normalize.to_s
+
+    word_html = URI.open(word_url)
+    word_doc = Nokogiri::HTML(word_html)
+
+    pronunciation = word_doc.at_css('.section.word .translation')&.text&.strip&.gsub('/', '')
+    parts_of_speech = word_doc.at_css('.section.word .description')&.text&.strip&.gsub(/[()]/, '')
+    definition = word_doc.at_css('.section.text-area p').text.strip
+    examples = word_doc.css('.section.list-w-title ul li')&.first&.text&.strip
+
+    {
+      part_of_speech: parts_of_speech,
+      pronunciation: pronunciation,
+      definition: definition,
+      example: examples
+    }
+  end
+
+  def url
+    "https://daily.wordreference.com/intermediate-word-of-the-day/"
+  end
+
+end
