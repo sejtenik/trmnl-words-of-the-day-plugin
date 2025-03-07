f688164b32d50c19c9e8e7ce008e544974b30000
sejtenik <sejtenik@gmail.com>
2025-03-07 21:15:43 +0100

    enhance oxford_parser

--- src/providers/english/oxford_parser.rb
+++ src/providers/english/oxford_parser.rb
@@ -8,14 +8,33 @@ class OxfordParser < EnglishWordProvider
     part_of_speech = doc.at_css(".wotdPos")&.text&.strip
     definition = doc.at_css(".wotdDef")&.text&.strip
 
-    link = doc.at_css(".wotd h3 a")['href']
+    link = url + doc.at_css(".wotd h3 a")['href'] + '?tl=true'
 
-    #TODO goto link and get pronunciation and example
+    word_doc = get_details_doc(link, true)
+
+    pronunciation = word_doc.at_css(".pronunciation-ipa")&.text&.strip&.gsub(/^\/|\/$/, '')
+
+    last_quote = word_doc.at_css('ol.quotation-container')&.css('li.quotation')&.last&.css('.quotation-text')&.text&.strip
+
+    frequency_element = word_doc.at_css('.frequency-indicator')
+    frequency_visual = ""
+
+    if frequency_element && frequency_element['aria-description']
+      frequency_description = frequency_element['aria-description']
+      if frequency_description =~ /Frequency band: (\d+) out of (\d+)/
+        frequency_value = $1.to_i
+        frequency_max = $2.to_i
+        frequency_visual = "●" * frequency_value + "○" * (frequency_max - frequency_value)
+      end
+    end
 
     {
       definition: definition,
       part_of_speech: part_of_speech,
-      url: url + link
+      pronunciation: pronunciation,
+      example: last_quote,
+      level: frequency_visual,
+      url: link
     }
   end
 
