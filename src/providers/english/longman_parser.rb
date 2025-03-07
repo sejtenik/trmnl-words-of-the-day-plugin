f1b23c410538b71333ce34953b755b8226f5cce8
sejtenik <sejtenik@gmail.com>
2025-03-07 20:33:46 +0100

    enhance longman_parser

--- src/providers/english/longman_parser.rb
+++ src/providers/english/longman_parser.rb
@@ -8,10 +8,26 @@ class LongmanParser < EnglishWordProvider
     definition = doc.at_css("#wotd .ldoceEntry .newline a")&.text&.strip
     link = doc.at_css("#wotd .title_entry a")['href']
 
-    #TODO go to link and get part of speech, pronunciation and example
+    word_doc = get_details_doc(link)
+
+    pronunciation = word_doc.at_css('.PRON')&.inner_html&.force_encoding("utf-8")&.strip
+
+    part_of_speech = word_doc.at_css('.POS')&.text&.strip
+
+    example_in_def = word_doc.at_css('.EXAMPLE')&.text&.strip
+    level = word_doc.at_css('.tooltip.LEVEL')&.inner_html&.force_encoding("utf-8")&.strip
+
+    corpus_example = word_doc.at_css('.cexa1g1.exa')&.inner_html&.force_encoding("utf-8")&.strip || ""
+    corpus_example = corpus_example.gsub(/•/, '').gsub(/<[^>]*>/, '').strip if corpus_example
+
+    example = corpus_example.empty? ? example_in_def : corpus_example
 
     {
       definition: definition,
+      pronunciation: pronunciation,
+      part_of_speech: part_of_speech,
+      example: example,
+      level: level,
       url: link
     }
   end
