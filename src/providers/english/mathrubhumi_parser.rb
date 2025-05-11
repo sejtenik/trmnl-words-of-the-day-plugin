c71c7d19d276f9d808e11cbcd3dff854e1941e9f
sejtenik <sejtenik@gmail.com>
2025-05-11 22:22:04 +0200

    Refactor MathrubhumiParser to improve word extraction.

--- src/providers/english/mathrubhumi_parser.rb
+++ src/providers/english/mathrubhumi_parser.rb
@@ -1,20 +1,28 @@
 class MathrubhumiParser < EnglishWordProvider
 
   def fetch_word
-    link = @doc.at_xpath('//a[h1[contains(text(), "Word of the day")]]')
-    link.at_xpath('h1').text[/'.*?'/]&.delete("'")&.strip&.downcase
+    link = link1
+
+    unless link.nil?
+      return link.at_xpath('h1')&.text[/'.*?'/]&.delete("'")&.strip&.downcase
+    end
+
+    link = link2
+
+    link.text.strip.match(/.*:.{2}(.*)/)[1].chop
   end
 
   def fetch_definitions
-    link = 'https://english.mathrubhumi.com' + @doc.at_xpath('//a[h1[contains(text(), "Word of the day")]]')['href']
+    link = (link1.nil? ? link2['href'] : 'https://english.mathrubhumi.com' + link1['href'])
 
     @word_doc = get_details_doc(link)
 
-    divs = @word_doc.css('div.mpp-story-content-details-main.my-3')
+    divs = @word_doc.css('div.mpp-story-content-details-main.my-3, div.article-body')
 
     meaning = nil
     example = nil
     pronunciation = nil
+    part_of_speech = nil
 
     divs.each_with_index do |div, index|
       case div.at_css('p strong')&.text
@@ -30,16 +38,40 @@ class MathrubhumiParser < EnglishWordProvider
       end
     end
 
+    if meaning.nil?
+      pronunciation = @word_doc.at_xpath("//p[strong[contains(text(), 'Pronunciation:')]]")&.text&.sub('Pronunciation:', '')&.strip
+
+      meaning_node = @word_doc.at_xpath("//p[strong[contains(text(), 'Meaning:')]]/following-sibling::p[1]")
+      meaning = meaning_node&.text&.strip
+
+      part_of_speech = meaning[/\b(noun|verb|adjective|adverb)\b/i]&.downcase
+
+      example_node = @word_doc.at_xpath("//p[strong[contains(text(), 'Examples from Literature:')]]/following-sibling::p[1]")
+      example = example_node&.text&.strip
+
+    end
+
     {
       definition: meaning,
       url: link,
       pronunciation: pronunciation,
-      example: example
+      example: example,
+      part_of_speech: part_of_speech
     }
   end
 
   def url
     "https://english.mathrubhumi.com/topics/tag/word_of_the_day"
+  end
+
+  private
+
+  def link2
+    @doc.at_xpath('//a[contains(text(), "Word of the Day")]')
+  end
+
+  def link1
+    @doc.at_xpath('//a[h1[contains(text(), "Word of the day")]]')
   end
 end
 
